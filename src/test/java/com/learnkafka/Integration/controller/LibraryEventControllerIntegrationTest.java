734a0072d38b378e39817a52b2d0d13f9e7d6353
any65889 <any65889@gmail.com>
2020-05-07 10:57:54 +0530

    Emmbedded Kafka Server Integration Test

--- src/test/java/com/learnkafka/Integration/controller/LibraryEventControllerIntegrationTest.java
+++ src/test/java/com/learnkafka/Integration/controller/LibraryEventControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.learnkafka.Integration.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,40 +13,47 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.learnkafka.LibraryEventsProducerApplication;
 import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvents;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LibraryEventsProducerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = { "some-new-event" }, partitions = 3)
+@TestPropertySource(properties = { "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+		"spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}" })
 public class LibraryEventControllerIntegrationTest {
 
 	@Autowired
 	TestRestTemplate testRestTemplate;
-	
+
 	@Test
 	void postLibEvent() {
-		
+
 		Book book = new Book();
-		
+
 		book.setBookAuthor("Ankit");
 		book.setBookId(32);
 		book.setBookName("BEST Seller");
 
-		
 		LibraryEvents libevents = new LibraryEvents();
 		libevents.setBook(book);
-		libevents.setLibraryEventId(98);	
-		
+		libevents.setLibraryEventId(98);
+
 		HttpHeaders headers = new HttpHeaders();
-		
+
 		headers.set("content-type", MediaType.APPLICATION_JSON.toString());
-		
-		HttpEntity<LibraryEvents> request = new HttpEntity<>(libevents,headers);
-		
-		ResponseEntity<LibraryEvents> response =   testRestTemplate.exchange("/dev/libraryevents", HttpMethod.POST,request,LibraryEvents.class);
-		
+
+		HttpEntity<LibraryEvents> request = new HttpEntity<>(libevents, headers);
+
+		ResponseEntity<LibraryEvents> response = testRestTemplate.exchange("/dev/libraryevents", HttpMethod.POST,
+				request, LibraryEvents.class);
+
 		assertEquals(HttpStatus.CREATED, response.getStatusCode());
-		
-		
+
 	}
 }
